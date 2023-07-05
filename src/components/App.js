6ab4f7c9b240ec6291c4ff1716cad288a1b21a01
Ahmad <khan@mail.ru>
2023-07-05 23:15:23 +0300

    some fixes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Users } from "./Users";
 import { Todos } from "./Todos";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { loadUsers } from "../redux/action";
 
@@ -10,13 +10,20 @@ import { loadUsers } from "../redux/action";
 
 
 export const App = () => {
+const loadingUsers = useSelector((state) => state.users.loading)
+const loadingTodos = useSelector((state) => state.users.loading)
+
 const dispatch = useDispatch()
 
 useEffect(() => {
   dispatch(loadUsers())
 }, [])
 
-
+if(loadingUsers || loadingTodos) {
+  return (
+    <h1>Идет загрузка...</h1>
+  )
+}
 
   return (
     <Routes>
